d97c8fa239232f29f73a52defb981857bc973994
Bevis <abc123123437@gmail.com>
2024-09-05 16:43:21 +0800

    chore: split file

--- cmd/publish_manager_service/handler/handler.go
+++ cmd/publish_manager_service/handler/handler.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -10,165 +9,10 @@ import (
 	"github.com/ray31245/seo_cluster/cmd/publish_manager_service/model"
 	aiAssistInterface "github.com/ray31245/seo_cluster/pkg/ai_assist/ai_assist_interface"
 	"github.com/ray31245/seo_cluster/pkg/util"
-	publishManager "github.com/ray31245/seo_cluster/service/publish_manager"
 	sitemanager "github.com/ray31245/seo_cluster/service/site_manager"
 
 	"github.com/gin-gonic/gin"
 )
-
-type PublishHandler struct {
-	publisher *publishManager.PublishManager
-}
-
-func NewPublishHandler(publisher *publishManager.PublishManager) *PublishHandler {
-	return &PublishHandler{
-		publisher: publisher,
-	}
-}
-
-func (p *PublishHandler) AveragePublishHandler(c *gin.Context) {
-	// get data body from request
-	req := model.PublishArticleRequest{}
-
-	err := c.ShouldBind(&req)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": fmt.Sprintf("error: %v", err),
-		})
-
-		return
-	}
-
-	// check data
-	if req.Title == "" || req.Content == "" {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": fmt.Sprintf("error: %v", "data is not complete"),
-		})
-
-		return
-	}
-
-	req.Content, err = util.DecodeImageListDivFromHTMl([]byte(req.Content))
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": fmt.Sprintf("error: %v", err),
-		})
-
-		return
-	}
-
-	err = p.publisher.AveragePublish(c, req.ToZBlogAPI())
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": fmt.Sprintf("error: %v", err),
-		})
-
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"message": "ok",
-	})
-}
-
-func (p *PublishHandler) PrePublishHandler(c *gin.Context) {
-	// get data body from request
-	req := model.PublishArticleRequest{}
-
-	err := c.ShouldBind(&req)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": fmt.Sprintf("error: %v", err),
-		})
-
-		return
-	}
-
-	// check data
-	if req.Title == "" || req.Content == "" {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": fmt.Sprintf("error: %v", "data is not complete"),
-		})
-
-		return
-	}
-
-	req.Content, err = util.DecodeImageListDivFromHTMl([]byte(req.Content))
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": fmt.Sprintf("error: %v", err),
-		})
-
-		return
-	}
-
-	err = p.publisher.PrePublish(req.ToZBlogAPI())
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": fmt.Sprintf("error: %v", err),
-		})
-
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"message": "ok",
-	})
-}
-
-func (p *PublishHandler) FlexiblePublishHandler(c *gin.Context) {
-	// get data body from request
-	req := model.PublishArticleRequest{}
-
-	err := c.ShouldBind(&req)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": fmt.Sprintf("error: %v", err),
-		})
-
-		return
-	}
-
-	// check data
-	if req.Title == "" || req.Content == "" {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": fmt.Sprintf("error: %v", "data is not complete"),
-		})
-
-		return
-	}
-
-	req.Content, err = util.DecodeImageListDivFromHTMl([]byte(req.Content))
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": fmt.Sprintf("error: %v", err),
-		})
-
-		return
-	}
-
-	err = p.publisher.AveragePublish(c, req.ToZBlogAPI())
-	if errors.Is(err, publishManager.ErrNoCategoryNeedToBePublished) {
-		err = p.publisher.PrePublish(req.ToZBlogAPI())
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"message": fmt.Sprintf("error: %v", err),
-			})
-
-			return
-		}
-	} else if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": fmt.Sprintf("error: %v", err),
-		})
-
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"message": "ok",
-	})
-}
 
 type SiteHandler struct {
 	sitemanager *sitemanager.SiteManager
---
+++ cmd/publish_manager_service/handler/publish.go
@@ -0,0 +1,166 @@
+package handler
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ray31245/seo_cluster/cmd/publish_manager_service/model"
+	"github.com/ray31245/seo_cluster/pkg/util"
+	publishManager "github.com/ray31245/seo_cluster/service/publish_manager"
+)
+
+type PublishHandler struct {
+	publisher *publishManager.PublishManager
+}
+
+func NewPublishHandler(publisher *publishManager.PublishManager) *PublishHandler {
+	return &PublishHandler{
+		publisher: publisher,
+	}
+}
+
+func (p *PublishHandler) AveragePublishHandler(c *gin.Context) {
+	// get data body from request
+	req := model.PublishArticleRequest{}
+
+	err := c.ShouldBind(&req)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": fmt.Sprintf("error: %v", err),
+		})
+
+		return
+	}
+
+	// check data
+	if req.Title == "" || req.Content == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": fmt.Sprintf("error: %v", "data is not complete"),
+		})
+
+		return
+	}
+
+	req.Content, err = util.DecodeImageListDivFromHTMl([]byte(req.Content))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": fmt.Sprintf("error: %v", err),
+		})
+
+		return
+	}
+
+	err = p.publisher.AveragePublish(c, req.ToZBlogAPI())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": fmt.Sprintf("error: %v", err),
+		})
+
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+	})
+}
+
+func (p *PublishHandler) PrePublishHandler(c *gin.Context) {
+	// get data body from request
+	req := model.PublishArticleRequest{}
+
+	err := c.ShouldBind(&req)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": fmt.Sprintf("error: %v", err),
+		})
+
+		return
+	}
+
+	// check data
+	if req.Title == "" || req.Content == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": fmt.Sprintf("error: %v", "data is not complete"),
+		})
+
+		return
+	}
+
+	req.Content, err = util.DecodeImageListDivFromHTMl([]byte(req.Content))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": fmt.Sprintf("error: %v", err),
+		})
+
+		return
+	}
+
+	err = p.publisher.PrePublish(req.ToZBlogAPI())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": fmt.Sprintf("error: %v", err),
+		})
+
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+	})
+}
+
+func (p *PublishHandler) FlexiblePublishHandler(c *gin.Context) {
+	// get data body from request
+	req := model.PublishArticleRequest{}
+
+	err := c.ShouldBind(&req)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": fmt.Sprintf("error: %v", err),
+		})
+
+		return
+	}
+
+	// check data
+	if req.Title == "" || req.Content == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": fmt.Sprintf("error: %v", "data is not complete"),
+		})
+
+		return
+	}
+
+	req.Content, err = util.DecodeImageListDivFromHTMl([]byte(req.Content))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": fmt.Sprintf("error: %v", err),
+		})
+
+		return
+	}
+
+	err = p.publisher.AveragePublish(c, req.ToZBlogAPI())
+	if errors.Is(err, publishManager.ErrNoCategoryNeedToBePublished) {
+		err = p.publisher.PrePublish(req.ToZBlogAPI())
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": fmt.Sprintf("error: %v", err),
+			})
+
+			return
+		}
+	} else if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": fmt.Sprintf("error: %v", err),
+		})
+
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+	})
+}
